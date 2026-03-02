134a7e13602a44c4376ffce59654282862f1e432
KinhKhaVN <pebin0123@gmail.com>
2026-03-02 21:36:08 +0700

    Implement the Pattern

---
+++ Singleton/DataBase/main.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+#include "Database.h"
+
+
+int main () {
+
+  Database *DB;
+  char * name = new char[20];
+  char * otherDatabase = new char[20];
+
+  std::strcpy(name, "Student");
+  std::strcpy(otherDatabase, "Class");
+
+  DB = Database::getInstance(name);
+  puts(DB->getName());
+  std::cout << DB << '\n';
+  DB = Database::getInstance(otherDatabase);
+  puts(DB->getName());
+  std::cout << DB << '\n';
+
+  delete DB;
+
+  return 19;
+}
